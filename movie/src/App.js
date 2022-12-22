617bba18b01416c3dd675069d3b747d3b2a4e89e
graykeng <graykeng7@gmail.com>
2022-12-22 11:24:02 -0800

    trying for fetch data from OMDB

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+const API_URL = 'http://www.omdbapi.com?apikey=7d47594';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch (`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data);
+    }
+    
+    useEffect(() => {
+        searchMovies("Spiderman");
+    }, [])
+
     return (
         <h1>test</h1>
     )
